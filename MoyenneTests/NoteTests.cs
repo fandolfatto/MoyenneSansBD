88079ab5e1a8df050c8400ad67de2e09c94a99da
CPNV\Frederique.ANDOLFATT <frederique.andolfatto@cpnv.ch>
2017-10-02 13:01:12 +0200

    modification projet tests

--- MoyenneTests/NoteTests.cs
+++ MoyenneTests/NoteTests.cs
@@ -5,17 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Moyenne.Tests
 {
     [TestClass()]
     public class NoteTests
     {
-        [TestMethod()]
-        public void CalculMoyenneTest()
-        {
-
-        }
 
         [TestMethod()]
         public void NoteTest()
@@ -30,5 +26,30 @@ namespace Moyenne.Tests
             //test
             Assert.AreEqual(noteCalcule, noteAttendu);
         }
+
+        [TestMethod()]
+        public void CalculMoyenneTest()
+        {
+            //preparation des données
+            ListBox lstNote = new ListBox();
+            Note note1 = new Note(50);
+            lstNote.Items.Add(note1);
+            Note note2 = new Note(55);
+            lstNote.Items.Add(note2);
+            Note note3 = new Note(45);
+            lstNote.Items.Add(note3);
+            Note note4 = new Note(47);
+            lstNote.Items.Add(note4);
+
+            float moyenneAttendue = 4.93F;
+
+            //execution du code à tester
+            float moyenneCalcule = Note.CalculMoyenne(lstNote);
+
+            //test
+            Assert.AreEqual(moyenneCalcule, moyenneAttendue);
+        }
+
+        
     }
 }
